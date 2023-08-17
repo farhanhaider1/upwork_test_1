0e8b9defc5dc42a0876aab55c1e580662863c89f
Farhan Haider <57194926+farhanhaider1@users.noreply.github.com>
2023-08-17 09:39:10 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,144 @@
+import React, { useState } from "react";
+import { createRoot } from "react-dom/client";
+
+const style = {
+  table: {
+    borderCollapse: "collapse",
+  },
+  tableCell: {
+    border: "1px solid gray",
+    margin: 0,
+    padding: "5px 10px",
+    width: "max-content",
+    minWidth: "150px",
+  },
+  form: {
+    container: {
+      padding: "20px",
+      border: "1px solid #F0F8FF",
+      borderRadius: "15px",
+      width: "max-content",
+      marginBottom: "40px",
+    },
+    inputs: {
+      marginBottom: "5px",
+    },
+    submitBtn: {
+      marginTop: "10px",
+      padding: "10px 15px",
+      border: "none",
+      backgroundColor: "lightseagreen",
+      fontSize: "14px",
+      borderRadius: "5px",
+    },
+  },
+};
+
+function PhoneBookForm({ addEntryToPhoneBook }) {
+  const [firstName, setFirstName] = useState("Coder");
+  const [lastName, setLastName] = useState("Byte");
+  const [phone, setPhone] = useState("8885559999");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addEntryToPhoneBook({ firstName, lastName, phone });
+  };
+
+  return (
+    <form onSubmit={handleSubmit} style={style.form.container}>
+      <label>First name:</label>
+      <br />
+      <input
+        style={style.form.inputs}
+        className="userFirstname"
+        name="userFirstname"
+        type="text"
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+      />
+      <br />
+      <label>Last name:</label>
+      <br />
+      <input
+        style={style.form.inputs}
+        className="userLastname"
+        name="userLastname"
+        type="text"
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+      />
+      <br />
+      <label>Phone:</label>
+      <br />
+      <input
+        style={style.form.inputs}
+        className="userPhone"
+        name="userPhone"
+        type="text"
+        value={phone}
+        onChange={(e) => setPhone(e.target.value)}
+      />
+      <br />
+      <input
+        style={style.form.submitBtn}
+        className="submitButton"
+        type="submit"
+        value="Add User"
+      />
+    </form>
+  );
+}
+
+function InformationTable(props) {
+  return (
+    <table style={style.table} className="informationTable">
+      <thead>
+        <tr>
+          <th style={style.tableCell}>First name</th>
+          <th style={style.tableCell}>Last name</th>
+          <th style={style.tableCell}>Phone</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.phoneBook.map((data, index) => (
+          <tr key={index}>
+            <td style={style.tableCell}>{data.firstName}</td>
+            <td style={style.tableCell}>{data.lastName}</td>
+            <td style={style.tableCell}>{data.phone}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+function Application(props) {
+  const [phoneBook, setPhoneBook] = useState([]);
+
+  const addEntryToPhoneBook = (newEntry) => {
+    setPhoneBook((prevPhoneBookData) => {
+      const updatedPhoneBook = [...prevPhoneBookData, newEntry];
+      updatedPhoneBook.sort((a, b) => {
+        if (a.lastName < b.lastName) {
+          return -1;
+        }
+        if (a.lastName > b.lastName) {
+          return 1;
+        }
+        return 0;
+      });
+      return updatedPhoneBook;
+    });
+  };
+
+  return (
+    <section>
+      <PhoneBookForm addEntryToPhoneBook={addEntryToPhoneBook} />
+      <InformationTable phoneBook={phoneBook} />
+    </section>
+  );
+}
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<Application />);
